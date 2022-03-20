a386d5aaf6cde93ca09f159b333dd34edaa33a2f
Pavel <k.paul19.acc@gmail.com>
2022-03-21 00:26:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿SomeEventHandler handler = new();
+handler.OnKeyPressed += HandleKeyPressed;
+handler.Run();
+
+static void HandleKeyPressed(object? sender, char c)
+{
+    Console.WriteLine($" you pressed \"{c}\"");
+}
+
+class SomeEventHandler
+{
+    public event EventHandler<char>? OnKeyPressed;
+
+    public void Run()
+    {
+        var c = Console.ReadKey();
+        while (c.Key != ConsoleKey.C)
+        {
+            OnKeyPressed?.Invoke(this, c.KeyChar);
+            c = Console.ReadKey();
+        }
+    }
+}
